19ec61b5cac60c7ff2880c8a56b7e4655b1674f1
rtaph <rafael@pilliardhellwig.ca>
2014-10-25 13:27:31 +0300

    add first attempt of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        xinv <- NULL
+        set <- function(y = matrix()){
+          x <<- y
+          xinv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(somematrix) xinv <<- somematrix
+        getinv <- function() xinv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
@@ -12,4 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+        xinv <- x$getinv()
+        if(!is.null(xinv)) {
+          message("getting cached matrix inverse")
+          return(xinv)
+        }
+        data <- x$get()
+        xinv <- solve(data, ...)
+        x$setinv(xinv)
+        xinv
 }
